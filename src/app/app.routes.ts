7e91ae64720f9d96b814c589bd017276dfc37936
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2025-05-03 11:24:45 -0600

    route and root api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'session',
+        pathMatch: 'full'
+    },
 
     {   path: "session",
         loadChildren: () => import('./modules/session/auth.routes').then(m => m.routes)
