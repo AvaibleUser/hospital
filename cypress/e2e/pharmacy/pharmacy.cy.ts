bd30a7d3b578205b7c6413c68fdc438607e602e3
alodev <brayan.quialo@gmail.com>
2025-05-12 22:06:04 -0600

    feat(pharmacy E2E): Add test for creating a new medicine

---
+++ cypress/e2e/pharmacy/pharmacy.cy.ts
@@ -0,0 +1,37 @@
+describe('Pharmacy', () => {
+    it('Deberia crear un nuevo medicamento', () => {
+        cy.visit('http://localhost:4200/session/login');
+
+        // Llenar el formulario
+        cy.get('input[name="email"]').type('brayan.quialo@gmail.com'); //Este usuario es de rrhh
+        cy.get('input[name="password"]').type('contra123');
+
+        // Enviar el formulario
+        cy.get('button[type="submit"]').click();
+
+        // Esperar redirección al dashboard de administrador
+        cy.url().should('include', '/pharmacy');
+
+        cy.visit('http://localhost:4200/pharmacy/inventory');
+
+        //Esperar a que se cargue la pagina
+        cy.contains('Gestión de Inventario').should('exist');
+
+        //Crear un nuevo medicamento
+        cy.get('button[data-cy="create-medicine"]').click();
+
+        //Llenar el formulario
+        cy.contains("Nuevo Medicamento").should('exist');
+        cy.get('input[name="name"]').type('Producto de prueba');
+        cy.get('input[name="unitPrice"]').type('100.00');
+        cy.get('input[name="unitCost"]').type('60.00');
+        cy.get('input[name="stock"]').type('50');
+        cy.get('input[name="minStock"]').type('10');
+
+        //Crear el medicamento
+        cy.get('button[data-cy="create-medicine-confirm"]').click();
+
+        //Esperar a que se cargue el modal de respuesta
+        cy.contains('Medicamento creado correctamente').should('exist');
+    });
+});
